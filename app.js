2ddf16e2903b28c4dc0c2ae1af4180ed4212f659
Your Name <kula.jega@gmail.com>
2021-01-18 18:01:35 +0100

    test

--- app.js
+++ app.js
@@ -9,17 +9,16 @@ const HttpError = require('./models/http-error');
 const db = require('./setup/database');
 
 
-
 const usersRoutes = require("./modules/Users/router");
 const dogRoutes = require("./modules/Dogs/router");
 const vetRoutes = require("./modules/Veterinaries/router");
- const trainningRoutes = require("./modules/Trainings/router");
- const medicalRoutes = require("./modules/Medical/router");
+const trainningRoutes = require("./modules/Trainings/router");
+const medicalRoutes = require("./modules/Medical/router");
 
- const appointRoutes = require("./modules/Appoiments/router");
- const packRoutes = require("./modules/Packages/router");
- const lessonRoutes = require("./modules/Lesson/router");
- const refRoutes = require("./modules/Reference/router");
+const appointRoutes = require("./modules/Appoiments/router");
+const packRoutes = require("./modules/Packages/router");
+const lessonRoutes = require("./modules/Lesson/router");
+const refRoutes = require("./modules/Reference/router");
 
 
 
@@ -32,10 +31,10 @@ const vetRoutes = require("./modules/Veterinaries/router");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-	// Request body cookie parser
+// Request body cookie parser
 app.use(cookieParser());
 
-	app.use(morgan("tiny"));
+app.use(morgan("tiny"));
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 
@@ -46,7 +45,7 @@ app.use((req, res, next) => {
     "Origin, X-Requested-With, Content-Type, Accept"
   );
 
-  res.setHeader( "Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
@@ -55,14 +54,14 @@ app.use((req, res, next) => {
 
 app.use("/api/user", usersRoutes);
 app.use("/api/dog", dogRoutes);
- app.use("/api/vet", vetRoutes);
- app.use("/api/trainning", trainningRoutes);
- app.use("/api/medical", medicalRoutes);
- app.use("/api/appoint", appointRoutes);
- app.use("/api/pack", packRoutes);
- app.use("/api/lesson", lessonRoutes);
- app.use("/api/ref", refRoutes);
-app.use((req,res,next) => {
+app.use("/api/vet", vetRoutes);
+app.use("/api/trainning", trainningRoutes);
+app.use("/api/medical", medicalRoutes);
+app.use("/api/appoint", appointRoutes);
+app.use("/api/pack", packRoutes);
+app.use("/api/lesson", lessonRoutes);
+app.use("/api/ref", refRoutes);
+app.use((req, res, next) => {
   const error = new HttpError('Could not find this routes.', 404);
   throw error;
 });
