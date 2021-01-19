7b6455a9295cb33a484c1487cc9fa6bb76c3d370
Your Name <kula.jega@gmail.com>
2021-01-19 23:47:00 +0100

    delete user

--- modules/Users/router.js
+++ modules/Users/router.js
@@ -8,9 +8,7 @@ const UserController =require("./controller");
 //Public routes
 router.get("/:id",UserController.getUser);
 router.post("/authenticate",UserController.authenticate);
-
 router.post("/register", UserController.register);
-
 router.get("/vet/:id", UserController.getUserVet);
 router.delete("/:id", UserController.delete);
 router.put("/:id",UserController.editUser);
